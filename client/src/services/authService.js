17f192a74904e082175bde9b37efbbcb7d879a54
Suraj Sudhir Thoke <surajthoke06@gmail.com>
2025-10-28 00:58:43 +0530

    Update authService.js

--- client/src/services/authService.js
+++ client/src/services/authService.js
@@ -1,6 +1,7 @@
-import axios from 'axios';
+import axios from "axios";
 
-const API_URL = 'http://localhost:5000/api/auth';
+// Pick the backend dynamically based on environment
+const API_URL = import.meta.env.VITE_API_URL + "/api/auth";
 
 export const loginUser = async (email, password) => {
   const res = await axios.post(`${API_URL}/login`, { email, password });
